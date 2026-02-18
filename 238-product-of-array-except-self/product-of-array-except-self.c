4070c0d907958b2bb2cd90c2b4a8132d14184a40
ayushjoshi0978 <ayushjoshi044@gmail.com>
2026-02-18 19:27:58 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (92.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -1,25 +1,21 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <stdlib.h>
+
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
-    
     *returnSize = numsSize;
     
-    // Allocate result array
-    int* result = (int*)malloc(numsSize * sizeof(int));
+    int* res = (int*)malloc(numsSize * sizeof(int));
     
-    // Step 1: store left products
-    result[0] = 1;
-    for (int i = 1; i < numsSize; i++) {
-        result[i] = result[i - 1] * nums[i - 1];
+    int left = 1;
+    for (int i = 0; i < numsSize; i++) {
+        res[i] = left;
+        left *= nums[i];
     }
     
-    // Step 2: multiply with right products
-    int rightProduct = 1;
+    int right = 1;
     for (int i = numsSize - 1; i >= 0; i--) {
-        result[i] = result[i] * rightProduct;
-        rightProduct *= nums[i];
+        res[i] *= right;
+        right *= nums[i];
     }
     
-    return result;
+    return res;
 }
